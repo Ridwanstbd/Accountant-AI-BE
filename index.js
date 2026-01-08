9321c9785a85d901617651a16e6906ad3895cb61
ridwanstbd <ridwansetiobudi77@gmail.com>
2026-01-08 16:39:27 +0700

    mute rate limiter

--- index.js
+++ index.js
@@ -17,17 +17,17 @@ app.set("trust proxy", true);
 app.use(helmet());
 app.use(cors());
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 1000,
-  standardHeaders: true,
-  legacyHeaders: false,
-  keyGenerator: (req) => {
-    return req.ip || "global";
-  },
-});
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000,
+//   max: 1000,
+//   standardHeaders: true,
+//   legacyHeaders: false,
+//   keyGenerator: (req) => {
+//     return req.ip || "global";
+//   },
+// });
 
-app.use("/api", limiter);
+// app.use("/api", limiter);
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
